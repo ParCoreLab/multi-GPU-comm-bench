1e084d537fbf333ff24b1df4ec47666b87120331
emrgncr <genceremir@gmail.com>
2024-04-23 02:25:37 +0300

    trying to fix makefile

--- nvshmem/generic_defs.mk
+++ nvshmem/generic_defs.mk
@@ -3,8 +3,7 @@ OBJS := $(SRCS:.c=.o)
 OBJS := $(OBJS:.cu=.o)
 
 UTIL_SRCS := $(wildcard ../../util/*.c) $(wildcard ../../util/*.cu)
-UTIL_OBJS := $(patsubst %.c, %.o, $(UTIL_SRCS))
-UTIL_OBJS := $(patsubst %.cu, %.o, $(UTIL_OBJS))
+UTIL_OBJS := $(patsubst %.c, %.o, $(UTIL_SRCS)) $(patsubst %.cu, %.o, $(UTIL_SRCS))
 
 CUDA_UTIL_SRCS := $(wildcard ../../cuda_util/*.c)
 CUDA_UTIL_OBJS := $(patsubst %.c, %.o, $(CUDA_UTIL_SRCS))
