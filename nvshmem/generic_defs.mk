01ccca7f1a30ffcaa36c8a9256c3be1c8d5402a6
emrgncr <genceremir@gmail.com>
2024-04-23 02:22:56 +0300

    modify makefile to include cu files

--- nvshmem/generic_defs.mk
+++ nvshmem/generic_defs.mk
@@ -1,8 +1,10 @@
-SRCS := $(wildcard *.c)
+SRCS := $(wildcard *.c) $(wildcard *.cu)
 OBJS := $(SRCS:.c=.o)
+OBJS := $(OBJS:.cu=.o)
 
-UTIL_SRCS := $(wildcard ../../util/*.c)
+UTIL_SRCS := $(wildcard ../../util/*.c) $(wildcard ../../util/*.cu)
 UTIL_OBJS := $(patsubst %.c, %.o, $(UTIL_SRCS))
+UTIL_OBJS := $(patsubst %.cu, %.o, $(UTIL_OBJS))
 
 CUDA_UTIL_SRCS := $(wildcard ../../cuda_util/*.c)
 CUDA_UTIL_OBJS := $(patsubst %.c, %.o, $(CUDA_UTIL_SRCS))
